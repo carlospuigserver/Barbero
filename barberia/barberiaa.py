cebd861c78cbee535a56e6ee3f6a94d9e1cb3b57
carlospuigserver <cpuiggut@myuax.com>
2023-04-25 23:55:33 +0200

    dividido en carpetas, clase barberia, func entrar

---
+++ barberia/barberiaa.py
@@ -0,0 +1,16 @@
+import queue
+import threading
+import time
+
+class Barberia:
+    def __init__(self,Numsillas):
+        self.NumSillas=Numsillas
+        self.sillas=queue.Queue(Numsillas)
+
+    def entrar(self,cliente):
+        if self.sillas.full():
+            print(f"{cliente} dice : La berbería está llena, vuelvo luego")
+        else:
+            self.sillas.put(cliente)
+            print(f"{cliente} dice: Me siento en la silla a esperar")
+            self.sillas(cliente)
